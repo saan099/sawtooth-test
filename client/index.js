6caf201768f7a429697cefefa1753ecb5323f731
sankalp <sankalp.sharma@sofocle.com>
2018-05-21 18:14:12 +0530

    #1 tx and batch fields order

--- client/index.js
+++ client/index.js
@@ -37,8 +37,8 @@ const payload = {
     Name: 'foo',
     Value: 42
 }
-const payloadBytes =Buffer.from(JSON.stringify(payload))
-//const payloadBytes = cbor.encode(payload)
+//const payloadBytes =Buffer.from(JSON.stringify(payload))
+const payloadBytes = cbor.encode(payload)
 
 console.log("payload Bytes-",payloadBytes)
 
@@ -48,22 +48,18 @@ const {createHash} = require('crypto')
 const {protobuf} = require('sawtooth-sdk')
 
 const transactionHeaderBytes = protobuf.TransactionHeader.encode({
+    batcherPublicKey: signer.getPublicKey().asHex(),
+    dependencies: [],
     familyName: 'transfer-chain',
     familyVersion: '1.0',
     inputs: [],
+    nonce:getNonce(),
     outputs: [],
-    signerPublicKey: signer.getPublicKey().asHex(),
+    payloadSha512: createHash('sha512').update(payloadBytes).digest('hex'),
+    signerPublicKey: signer.getPublicKey().asHex()
     // In this example, we're signing the batch with the same private key,
     // but the batch can be signed by another party, in which case, the
     // public key will need to be associated with that key.
-    batcherPublicKey: signer.getPublicKey().asHex(),
-    // In this example, there are no dependencies.  This list should include
-    // an previous transaction header signatures that must be applied for
-    // this transaction to successfully commit.
-    // For example,
-    // dependencies: ['540a6803971d1880ec73a96cb97815a95d374cbad5d865925e5aa0432fcf1931539afe10310c122c5eaae15df61236079abbf4f258889359c4d175516934484a'],
-    dependencies: [],
-    payloadSha512: createHash('sha512').update(payloadBytes).digest('hex')
 }).finish()
 
 console.log("Transaction header- ",transactionHeaderBytes)
@@ -96,7 +92,9 @@ const batchSignature = signer.sign(batchHeaderBytes)
 const batch = protobuf.Batch.create({
     header: batchHeaderBytes,
     headerSignature: batchSignature,
-    transactions: transactions
+    transactions: transactions,
+    trace:true
+    
 });
 
 const batchListBytes = protobuf.BatchList.encode({
@@ -116,4 +114,10 @@ request.post({
     console.log(response.body)
 })
 
+
+function getNonce() {
+    var dateString = Date.now().toString(36).slice(-5);
+    var randomString = Math.floor(Math.random() * 46655).toString(36);
+    return dateString + ('00' + randomString).slice(-3);
+}
 
